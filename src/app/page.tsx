601b4002a7121ec76fdfab8a64c7f3481dd5bcaa
osamanisar05 <164087967+osamanisar05@users.noreply.github.com>
2024-11-16 13:00:35 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,105 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
+import { useEffect } from "react";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+interface CoinRates {
+  BTC: number;
+  ETH: number;
+  USDT: number;
+  LINK: number;
+  BNB: number;
+  XRP: number;
+  ADA: number;
+  DOGE: number;
+  SOL: number;
+  AVAX: number;
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+interface CoinMarketResponse {
+  rates: CoinRates;
+}
+
+
+export default function CoinMArket(){
+  const [data,setData] = useState<CoinMarketResponse | null>(null);
+  const API_KEY = 'f6e945ee7008cc33cbbe1fe6bfb099e1'
+  useEffect(
+    ()=>{
+
+      fetch(`http://api.coinlayer.com/live?access_key=${API_KEY}`)
+
+      .then((Response)=> Response.json()) 
+      .then((jsonConverted) => setData(jsonConverted))
+
+    
+
+    },[]);
+    
+
+    return(
+      <div className="Header">
+      <h1>CRYPTO CURRENCY MARKET RATES</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Crypto Currency</th>
+            <th>Rates</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Bitcoin</td>
+            <td>{data?.rates?.BTC}</td>
+          </tr>
+
+          <tr>
+            <td>Ethereum</td>
+            <td>{data?.rates?.ETH}</td>
+          </tr>
+
+          <tr>
+            <td>Tether</td>
+            <td>{data?.rates?.USDT}</td>
+          </tr>
+
+          <tr>
+            <td>Chainlink</td>
+            <td>{data?.rates?.LINK}</td>
+          </tr>
+
+          <tr>
+            <td>Binance Coin</td>
+            <td>{data?.rates?.BNB}</td>
+          </tr>
+
+          <tr>
+            <td>Ripple</td>
+            <td>{data?.rates?.XRP}</td>
+          </tr>
+
+          <tr>
+            <td>Cardano</td>
+            <td>{data?.rates?.ADA}</td>
+          </tr>
+
+          <tr>
+            <td>Dogecoin</td>
+            <td>{data?.rates?.DOGE}</td>
+          </tr>
+
+          <tr>
+            <td>Solana</td>
+            <td>{data?.rates?.SOL}</td>
+          </tr>
+
+          <tr>
+            <td>Avalanche</td>
+            <td>{data?.rates?.AVAX}</td>
+          </tr>
+
+       </tbody>
+       </table>
+      </div>
+    )
+  
 }
